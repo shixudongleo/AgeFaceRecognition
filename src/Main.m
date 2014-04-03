f01ccaa5d4de0d0cb633b5fae19042ec9db0d139
shixudongleo <shixudongleo@gmail.com>
2014-04-03 18:13:41 +0800

    complete hw part 1: PCA

--- src/Main.m
+++ src/Main.m
@@ -7,17 +7,55 @@
 cd ../data;
 addpath(pwd);
 cd ../src;
-
-% load data 
-[trainX trainY] = ReadData('../data/train.txt');
-[testX testY] = ReadData('../data/test.txt');
-
+close all;
+clear all;
 
 %%
 %---------------------------------------------------
 % 1. Principle Component Analysis
 %---------------------------------------------------
+close all;
+clear all;
+% load data 
+[trainX trainY] = ReadData('../data/train.txt');
+[testX testY] = ReadData('../data/test.txt');
 
+num_eig_faces = 10;
+PCs = PCA(trainX, num_eig_faces);
+for ii = 1:num_eig_faces
+   face = DisplayFace(PCs(:, ii)); 
+   imwrite(face, ['../data/ouput/eigenface_', num2str(ii), '.png'], 'PNG');
+end
+
+max_dim = 286;
+precisions = zeros(max_dim, 1);
+for ii = 1:max_dim
+% feature reduction
+T = PCA(trainX, ii);
+trainX_PCA = trainX * T;% transpose both side;
+testX_PCA = testX * T;% transpose both side;
+
+% KNN training
+knn_model = ClassificationKNN.fit(trainX_PCA, trainY);
+y_knn = predict(knn_model, testX_PCA);    
+
+% calculate precisions 
+accuracy = sum(testY == y_knn)/length(testY);
+fprintf('The accuracy for dim: %d is: %f\n', ii, accuracy);
+precisions(ii) = accuracy;
+end
+save('../data/ouput/eigen_accuracy.mat', 'precisions');
+
+
+%%
+%---------------------------------------------------
+% 2. Nonnegative Matrix Factorization
+%---------------------------------------------------
+close all;
+clear all;
+% load data 
+[trainX trainY] = ReadData('../data/train.txt');
+[testX testY] = ReadData('../data/test.txt');
 
 
 
