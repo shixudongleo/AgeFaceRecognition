46451beb1f3afe86e4f6a9a2f1729a525fa360f4
shixudongleo <shixudongleo@gmail.com>
2014-04-05 22:51:00 +0800

    complete LDA.m

--- src/LDA.m
+++ src/LDA.m
@@ -26,6 +26,8 @@ each_class_mu = zeros(d, C);
 each_class_cov = zeros(d, d, C);
 mu = mean(X, 2);
 S_w = zeros(d, d);
+S_t = zeros(d, d);
+S_b = zeros(d, d);
 
 % initialize sample size for each class size
 for ii = 1:C
@@ -43,22 +45,36 @@ for ii = 1:C
    each_class_mu(:, ii) = mu_c;
    
    tmp_sum = zeros(d, d);
-   for jj = 1:C
+   for jj = 1:each_class_size(ii)
       X_c_jj = X_c(:, jj);
       tmp_sum = tmp_sum +  X_c_jj*X_c_jj';
    end
    each_class_cov(:, :, ii) = 1/each_class_size(ii)*tmp_sum - mu_c*mu_c';
+%    for jj = 1:each_class_size(ii)
+%         X_c_jj = X_c(:, jj);
+%         diff  = X_c_jj - mu_c;
+%         tmp_sum = tmp_sum + diff*diff';
+%    end
+%    each_class_cov(:, :, ii) = 1/each_class_size(ii)*tmp_sum;
 end
 
 % calculate within class covariance
 for ii = 1:C
-   S_w = prior(ii)*each_class_cov(:, :, ii);
+   S_w = S_w + prior(ii)*each_class_cov(:, :, ii);
+end
+
+% calculate between class covariance
+for ii = 1:C
+   diff = each_class_mu(ii) - mu;
+   S_b = S_b +  prior(ii)*(diff*diff');
 end
 
 
-% calculae between class covariance (total covariance - with class
-% covariance)
-
-
+% calculate eigenvectors of (S_w)^-1*S_b
+[V D] = eig(S_b, S_w);
+eigenvalues = diag(D);
+[sorted_eig_val idx] = sort(eigenvalues, 'descend');
+V = V(:, idx);
+T = V(:, 1:num_basis);
 
 end
